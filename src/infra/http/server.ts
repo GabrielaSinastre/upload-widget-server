c96e98075f66c756895d67a6bb283fb5bdff959e
Gabriela Sinastre <55140599+GabrielaSinastre@users.noreply.github.com>
2026-05-20 13:55:17 -0300

    feat: docker compose settings and errors handler

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,34 @@
-import fastifyCors from '@fastify/cors'
+import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from '@/env'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // enviar o erro p/ algum ferramenta de observalidade (Sentry/Datadog/Grafana)
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
 //registrar plugin de cors e deixar todas as portas abertas para o frontend acessar a api
 server.register(fastifyCors, { origin: '*' })
+
+console.log(env.DATABASE_URL)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('Server is running on port 3333')

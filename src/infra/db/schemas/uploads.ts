26511f24f8b8f384275c598c56db4c6292309c70
GabrielaSinastre <gabrielamaria.gaby@hotmail.com>
2026-05-28 14:21:23 -0300

    feat: drizzle orm config

---
+++ src/infra/db/schemas/uploads.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { randomUUID } from "node:crypto";
+
+export const uploads = pgTable('uploads', {
+  id: text('id').primaryKey().$defaultFn(() => randomUUID()),
+  name: text('name').notNull(),
+  remoteKey: text('remote_key').notNull().unique(), //caminho remoto do arquivo
+  remoteUrl: text('remote_url').notNull(), //url de acesso ao arquivo, quando tiver arquivo publico
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+})
